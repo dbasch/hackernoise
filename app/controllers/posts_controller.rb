ef8d0fc9febe75fb549a3a20ddd555959cdfe393
Diego Basch <dbasch@gmail.com>
2011-06-28 19:11:16 -0700

    validate user before doing post actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_filter :authenticate_user!, :except => :show
   def create
     u = User.find(params[:post][:user])
     params[:post][:user] = u
